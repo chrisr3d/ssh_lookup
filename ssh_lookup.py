03e4d00496fd1dd0ec1b9087959aa01b03d4b318
Christian Studer <christian.studer@circl.lu>
2022-05-05 18:36:31 +0200

    add: Added the MISP connector to push network-connection & passive-dns objects gathered from SSH logs to MISP

--- ssh_lookup.py
+++ ssh_lookup.py
@@ -1,15 +1,21 @@
 import argparse
 import csv
 import json
-import logging
 import os
 import requests
 import sys
 from collections import defaultdict
+from datetime import datetime
 from pathlib import Path
 from pymisp import PyMISP, MISPEvent, MISPObject
 
 _CONFIG_PATH = Path(__file__).resolve().parent / 'config'
+_CONNECTION_OBJECT_FIELDS = ('dst_ip', 'dst_port', 'src_ip', 'src_port', 'timestamp')
+_CONNECTION_OBJECT_MAPPING = {
+    'dst_port': {'type': 'port', 'object_relation': 'dst-port'},
+    'src_ip': {'type': 'ip-src', 'object_relation': 'ip-src'},
+    'src_port': {'type': 'port', 'object_relation': 'src-port'}
+}
 _PASSIVE_SSH_FIELDS = ('banner', 'hassh', 'keys')
 
 
@@ -38,9 +44,45 @@ def _fix_nulls(csv_file):
         yield line.replace('\0', '')
 
 
-def _perform_queries(filename, honeypot_name, passive_ssh_url, authentication):
+def _parse_destination(destination):
+    if ':' not in destination and any(letter.isalpha() for letter in destination):
+        return {'type': 'hostname', 'object_relation': 'hostname-dst'}
+    return {'type': 'ip-dst', 'object_relation': 'ip-dst'}
+
+
+def _parse_input_args(args):
+    if args.csvinput is not None:
+        return args.csvinput, _perform_csv_queries, [args.name]
+    return args.jsoninput, _perform_json_queries, []
+
+
+def _parse_status_code(filename, parsed_files, status_code, savejson):
+    if status_code == 401:
+        _clean_tmp_files(savejson, parsed_files)
+        sys.exit('Authentication error, please check your config file.', file=sys.stderr)
+    if status_code == 404:
+        _clean_tmp_files(savejson, parsed_files)
+        sys.exit()
+    if status_code == 0:
+        print(f'No Passive-SSH record found for the SSH logs from {filename}')
+    else:
+        print(f"Passive-SSH records from {filename} successfully saved in {filename}.passivessh.json")
+
+
+def _perform_csv_queries(filename, honeypot_name, passive_ssh_url, authentication):
+    ssh_logs = tuple(_csv_to_json(filename, honeypot_name))
     _write_json_logs(filename, honeypot_name)
-    ip_addresses = {log['src_ip'] for log in logs}
+    return _perform_queries(ssh_logs, filename, passive_ssh_url, authentication)
+
+
+def _perform_json_queries(filename, passive_ssh_url, authentication):
+    with open(filename, 'rt', encoding='utf-8') as f:
+        ssh_logs = json.loads(f.read())
+    _perform_queries(ssh_logs, filename, passive_ssh_url, authentication)
+
+
+def _perform_queries(ssh_logs, filename, passive_ssh_url, authentication):
+    ip_addresses = {log['src_ip'] for log in ssh_logs}
     passive_ssh_records = {}
     for ip_address in ip_addresses:
         try:
@@ -64,56 +106,169 @@ def _perform_queries(filename, honeypot_name, passive_ssh_url, authentication):
     return 0
 
 
-def _push_misp_data(parsed_files):
+def _push_misp_data(parsed_files, feature):
     first_seen = float('inf')
     last_seen = 0
-
+    misp_event = MISPEvent()
+    connections = defaultdict(list)
+    passive_ssh_records = {}
+    for filename in parsed_files:
+        with open(filename, 'rt', encoding='utf-8') as f:
+            ssh_logs = json.loads(f.read())
+        for log in ssh_logs:
+            if 'dst_ip' in log:
+                connections[log['src_ip']].append(
+                    {key: value for key, value in log.items() if key in _CONNECTION_OBJECT_FIELDS}
+                )
+        try:
+            passive_name = filename if feature == 'json' else filename[:-5]
+            with open(f'{passive_name}.passivessh.json', 'rt', encoding='utf-8') as f:
+                passivessh_record = json.loads(f.read())
+        except FileNotFoundError:
+            print(f'Error loading the Passive-SSH records file: {filename[:-5]}.passivessh.json', file=sys.stderr)
+            continue
+        for ip_address, record in passivessh_record.items():
+            passive_ssh_records[ip_address] = record
+    for ip_address, network_connections in connections.items():
+        connection_uuids = []
+        for network_connection in network_connections:
+            connection_object = MISPObject('network-connection')
+            dst_ip = network_connection['dst_ip']
+            attribute = _parse_destination(dst_ip)
+            attribute['value'] = dst_ip
+            connection_object.add_attribute(**attribute)
+            for feature, attribute in _CONNECTION_OBJECT_MAPPING.items():
+                if feature in network_connection:
+                    misp_attribute = {'value': network_connection[feature].replace(',', '')}
+                    misp_attribute.update(attribute)
+                    connection_object.add_attribute(**misp_attribute)
+            connection_object.add_attribute(
+                **{
+                    'type': 'datetime',
+                    'object_relation': 'first-packet-seen',
+                    'value': network_connection['timestamp']
+                }
+            )
+            timestamp = _date_to_timestamp(network_connection['timestamp'])
+            if timestamp > last_seen:
+                last_seen = timestamp
+            if timestamp < first_seen:
+                first_seen = timestamp
+            connection_object.add_attribute(
+                **{
+                    'type': 'text',
+                    'object_relation': 'layer7-protocol',
+                    'value': 'SSH'
+                }
+            )
+            misp_event.add_object(connection_object)
+            connection_uuids.append(connection_object.uuid)
+        if ip_address in passive_ssh_records:
+            record = passive_ssh_records[ip_address]
+            passive_ssh = MISPObject('passive-ssh')
+            passive_ssh.add_attribute(
+                **{
+                    'type': 'ip-dst',
+                    'object_relation': 'host',
+                    'value': ip_address
+                }
+            )
+            if 'keys' in record:
+                for key in record['keys']:
+                    passive_ssh.add_attribute(
+                        **{
+                            'type': 'ssh-fingerprint',
+                            'object_relation': 'fingerprint',
+                            'value': key['fingerprint']
+                        }
+                    )
+            if 'hassh' in record:
+                for hassh in record['hassh']:
+                    passive_ssh.add_attribute(
+                        **{
+                            'type': 'hassh-md5',
+                            'object_relation': 'hassh',
+                            'value': hassh
+                        }
+                    )
+            for feature in ('first_seen', 'last_seen'):
+                passive_ssh.add_attribute(
+                    **{
+                        'type': 'datetime',
+                        'object_relation': feature,
+                        'value': record[feature]
+                    }
+                )
+            for connection_uuid in connection_uuids:
+                passive_ssh.add_reference(connection_uuid, 'related-to')
+            misp_event.add_object(passive_ssh)
     with open(_CONFIG_PATH / 'misp_config.json', 'rt', encoding='utf-8') as f:
         misp_config = json.loads(f.read())
     misp_url = misp_config['MISP_url']
     misp_key = misp_config['MISP_automation_key']
     misp_verifycert = misp_config['verify_certificate']
+    first_seen = _timestamp_to_str(first_seen)
+    last_seen = _timestamp_to_str(last_seen)
+    misp_event.info = f'SSH logs captured between {first_seen} and {last_seen}'
     try:
         misp = PyMISP(misp_url, misp_key, misp_verifycert)
     except Exception as e:
+        import uuid
+        misp_file = Path(__file__).resolve().parent / 'tmp' / f'{uuid.uuid4()}.json'
+        with open(misp_file, 'wt', encoding='utf-8') as f:
+            f.write(misp_event.to_json(indent=4))
         print(f'Error while connecting to your MISP instance: {e}\nSaving MISP event in {misp_file}.', file=sys.stderr)
         return
+    misp.add_event(misp_event)
 
 
-def _write_json_logs(filename, honeypot_name):
-    logs = tuple(_csv_to_json(filename, honeypot_name))
+
+def _date_to_timestamp(date):
+    for ordinal in ('th', 'st', 'rd'):
+        if ordinal in date:
+            break
+    return datetime.strptime(date, f'%B %d{ordinal} %Y, %H:%M:%S.%f').timestamp()
+
+
+def _timestamp_to_str(timestamp):
+    return datetime.strftime(datetime.utcfromtimestamp(int(timestamp)), '%Y-%m-%d %H:%M:%S')
+
+
+def _write_json_logs(ssh_logs, filename):
     with open(f'{filename}.json', 'wt', encoding='utf-8') as f:
-        f.write(json.dumps(logs, indent=4))
+        f.write(json.dumps(ssh_logs, indent=4))
 
 
-def parse_csv(args):
+def parse_logs(args):
     parsed_files = []
-    if args.input is not None:
+    if args.passivessh_input is not None:
+        feature = 'pcap'
+        for filename in args.passivessh_input:
+            csv_filename = filename[:-16]
+            ssh_logs = tuple(_csv_to_json(filename, args.name))
+            _write_json_logs(ssh_logs, csv_filename)
+            parsed_files.append(f'{csv_filename}.json')
+    else:
         with open(_CONFIG_PATH / 'passive_ssh_config.json', 'rt', encoding='utf-8') as f:
             passive_ssh_config = json.loads(f.read())
             passive_ssh_url = passive_ssh_config['passive_ssh_url']
             authentication = (passive_ssh_config['api_user'], passive_ssh_config['api_key'])
-        for filename in args.input:
-            filename = Path(filename).resolve()
-            status_code = _perform_queries(filename, args.name, passive_ssh_url, authentication)
-            parsed_files.append(f'{filename}.json')
-            if status_code == 401:
-                _clean_tmp_files(args.savejson, parsed_files)
-                sys.exit('Authentication error, please check your config file.', file=sys.stderr)
-            if status_code == 404:
-                _clean_tmp_files(args.savejson, parsed_files)
-                sys.exit()
-            if status_code == 0:
-                print(f'No Passive-SSH record found for the SSH logs from {filename}')
-            else:
-                print(f"Passive-SSH records from {filename} successfully saved in {filename}.passivessh.json")
-    else:
-        for filename in args.passivessh_input:
-            csv_filename = filename[:-16]
-            _write_json_logs(csv_filename, args.name)
-            parsed_files.append(f'{csv_filename}.json')
+        if args.csvinput is not None:
+            feature = 'csv'
+            for filename in args.csvinput:
+                filename = Path(filename).resolve()
+                status_code = _perform_csv_queries(filename, args.name, passive_ssh_url, authentication)
+                parsed_files.append(f"{filename}.json")
+                _parse_status_code(filename, parsed_files, status_code, args.savejson)
+        else:
+            feature = 'json'
+            for filename in args.jsoninput:
+                filename = Path(filename).resolve()
+                status_code = _perform_json_queries(filename, passive_ssh_url, authentication)
+                parsed_files.append(filename)
+                _parse_status_code(filename, parsed_files, status_code, args.savejson)
     if args.misp:
-        _push_misp_data(parsed_files)
+        _push_misp_data(parsed_files, feature)
     _clean_tmp_files(args.savejson, parsed_files)
 
 
@@ -125,14 +280,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Perform lookups on a Passive-SSH platform.')
     subparsers = parser.add_subparsers()
 
-    csv_parser = subparsers.add_parser('csv', help='Parse SSH logs in CSV format.')
-    input_parser = csv_parser.add_mutually_exclusive_group(required=True)
-    input_parser.add_argument('-i', '--input', nargs='+', help='CSV input file(s).')
+    logs_parser = subparsers.add_parser('logs', help='Parse SSH logs in CSV format.')
+    input_parser = logs_parser.add_mutually_exclusive_group(required=True)
+    input_parser.add_argument('-c', '--csvinput', nargs='+', help='CSV input file(s).')
+    input_parser.add_argument('-j', '--jsoninput', nargs='+', help='JSON input file(s).')
     input_parser.add_argument('-p', '--passivessh_input', nargs='+', help='Passive-SSH record already queried input file(s).')
-    csv_parser.add_argument('-n', '--name', default='cowrie', help='Honeypot name.')
-    csv_parser.add_argument('--misp', action='store_true', help='Submit lookup results to MISP.')
-    csv_parser.add_argument('--savejson', action='store_true', help='Save JSON converted data.')
-    csv_parser.set_defaults(func=parse_csv)
+    logs_parser.add_argument('-n', '--name', default='cowrie', help='Honeypot name.')
+    logs_parser.add_argument('--misp', action='store_true', help='Submit lookup results to MISP.')
+    logs_parser.add_argument('--savejson', action='store_true', help='Save JSON converted data.')
+    logs_parser.set_defaults(func=parse_logs)
 
     pcap_parser = subparsers.add_parser('pcap', parents=[parser], add_help=False, help='Parse SSH packets from pcap file(s).')
     pcap_parser.add_argument('-i', '--input', nargs='+', required=True, help='PCAP input file(s).')
